47dfb4786e39e1c506ea9edac63ab8cd49df52fc
jp9863 <32777244+jp9863@users.noreply.github.com>
2017-10-26 17:30:30 -0400

    Add files via upload
    codons switched

---
+++ CodonFix.py
@@ -0,0 +1,48 @@
+'''
+@Jennifer Pfaff
+'''
+
+codon = {"K": "AAA", "N": "AAC", "K": "AAG", "N": "AAU",
+         "T": "ACA", "T": "ACC", "T": "ACG", "T": "ACU",
+         "R": "AGA", "S": "AGC", "R": "AGG", "S": "AGU",
+         "I": "AUA", "I": "AUC", "M": "AUG", "I": "AUU",
+
+         "Q": "CAA", "H": "CAC", "Q": "CAG", "H": "CAU,",
+         "P": "CCA", "P": "CCC", "P": "CCG", "P": "CCU",
+         "R": "CGA", "R": "CGC", "R": "CGG", "R": "CGU",
+         "L": "CUA", "L": "CUC", "L": "CUG", "L": "CUU",
+
+         "E": "GAA", "D": "GAC", "E": "GAG", "D": "GAU",
+         "A": "GCA", "A": "GCC", "A": "GCG", "A": "GCU",
+         "G": "GGA", "G": "GGC", "G": "GGG", "G": "GGU",
+         "V": "GUA", "V": "GUC", "V": "GUG", "V": "GUU",
+
+         "_": "UAA", "Y": "UAC", "_": "UAG", "T": "UAU",
+         "S": "UCA", "S": "UCC", "S": "UCG", "S": "UCU",
+         "_": "UGA", "C": "UGC", "W": "UGG", "C": "UGU",
+         "L": "UUA", "F": "UUC", "L": "UUG", "F": "UUU"}
+
+
+'''
+From original by Emely
+#codon hashmap
+    codon = {"AAA":"K", "AAC":"N", "AAG":"K", "AAU":"N",
+                "ACA":"T", "ACC":"T", "ACG":"T", "ACU":"T",
+                "AGA":"R", "AGC":"S", "AGG":"R", "AGU":"S",
+                "AUA":"I", "AUC":"I", "AUG":"M", "AUU":"I",
+
+                "CAA":"Q", "CAC":"H", "CAG":"Q", "CAU":"H",
+                "CCA":"P", "CCC":"P", "CCG":"P", "CCU":"P",
+                "CGA":"R", "CGC":"R", "CGG":"R", "CGU":"R",
+                "CUA":"L", "CUC":"L", "CUG":"L", "CUU":"L",
+
+                "GAA":"E", "GAC":"D", "GAG":"E", "GAU":"D",
+                "GCA":"A", "GCC":"A", "GCG":"A", "GCU":"A",
+                "GGA":"G", "GGC":"G", "GGG":"G", "GGU":"G",
+                "GUA":"V", "GUC":"V", "GUG":"V", "GUU":"V",
+
+                "UAA":"_", "UAC":"Y", "UAG":"_", "UAU":"T",
+                "UCA":"S", "UCC":"S", "UCG":"S", "UCU":"S",
+                "UGA":"_", "UGC":"C", "UGG":"W", "UGU":"C",
+                "UUA":"L", "UUC":"F", "UUG":"L", "UUU":"F"}
+'''
