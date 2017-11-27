f19557df3e92d12d164b3b228dc59b6ca01cf625
Aidan Klein <aidan@shader.me>
2017-11-27 13:11:38 -0500

    Added sequence generator

--- resolver.py
+++ resolver.py
@@ -10,39 +10,21 @@ class UnmappedProteinError(Exception):
     pass
 
 
-"""
-Returns the probability of a given codon in the sequence given the entire sequence
-"""
-def compute_codon_probability(index, protein_sequence):
-    return 1
-
-
-"""
-Returns the number of possible sequences
-"""
-def count_possible_sequences(sequence):
-    num_possible = 1
-    for i, p in enumerate(sequence):
-        if p not in inverse_codon_map:
-            raise UnmappedProteinError('{} has no mapping or is invalid'.format(p))
-        entry = inverse_codon_map[p]
-        if type(entry) is str:
-            continue
-        num_possible *= len(inverse_codon_map[p])
-    return num_possible
-
-
-"""
-Generates a DNA sequence from a given protein sequence and the sequence possibility number
-"""
-def generate_sequence(sequence, num):
-    return []
+def generate_sequences(sequence: list) -> list:
+    """
+    Generates a list of DNA sequences from a given protein sequence
+    """
+    sub_sequences = ['']
+    if len(sequence) > 1:
+        sub_sequences = generate_sequences(sequence[:-1])
+    gen_sequences = []
+    for codon in inverse_codon_map[sequence[0]]:
+        for sub_sequence in sub_sequences:
+            gen_sequences.append(codon + sub_sequence)
+    return gen_sequences
 
 
 if __name__ == '__main__':
     proteins = input("Proteins (single letter notation)> ")
     proteins = [p for p in list(proteins.upper()) if p in inverse_codon_map.keys()]
-    num_possible = count_possible_sequences(proteins)
-    print("Found {} possible DNA sequences".format(num_possible))
-    for sequence_num in range(num_possible):
-        print(generate_sequence(proteins, sequence_num))
+    print(generate_sequences(proteins))
